b7030d9e4759b5c5c573a5c7c8a64f20466747fd
Julian Acosta <julacosta9@gmail.com>
2021-11-05 02:01:33 -0700

    add Brackets brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./src/pages/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        bracketsBlue: "#2885F6",
+        bracketsGray: "#767D84",
+        bracketsOrange: "#F65928",
+      },
       screens: {
         content: "1169px",
       },
