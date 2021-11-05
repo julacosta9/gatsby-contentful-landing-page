0eb33b9c2483b2d906604149bea224f13ecd14a8
Julian Acosta <julacosta9@gmail.com>
2021-11-05 01:47:14 -0700

    complete initial design and layout of navbar

--- src/components/sections/Navbar.js
+++ src/components/sections/Navbar.js
@@ -1,25 +1,72 @@
 import React from "react";
-// import { graphql, useStaticQuery } from "gatsby";
+import { Link, graphql, useStaticQuery } from "gatsby";
+import arrow from "../../images/arrow.svg";
 
 const Header = () => {
-  // const titles = useStaticQuery(graphql`
-  //   query {
-  //     allContentfulCourse {
-  //       edges {
-  //         node {
-  //           title
-  //         }
-  //       }
-  //     }
-  //   }
-  // `);
+  const data = useStaticQuery(graphql`
+    query {
+      contentfulMainNavigation {
+        logo {
+          ... on ContentfulAsset {
+            description
+            file {
+              url
+            }
+          }
+        }
+        navigationLinks {
+          text
+          url
+        }
+        ctaText
+        ctaUrl
+        ctaColor
+      }
+    }
+  `);
 
   return (
     <header>
-      Hi!
-      {/* {titles.allContentfulCourse.edges.map((edge) => {
-        return <div>{edge.node.title}</div>;
-      })} */}
+      <div className="bg-transparent">
+        <div className="max-w-screen-content w-full mx-auto">
+          <div className="flex justify-between w-full bg-transparent py-4">
+            <Link className="flex items-center" to="/">
+              <img
+                src={data.contentfulMainNavigation.logo.file.url}
+                alt={data.contentfulMainNavigation.logo.description}
+              />
+            </Link>
+            <nav className="flex gap-8 items-center">
+              {data.contentfulMainNavigation.navigationLinks.map(
+                (link, index) => (
+                  <Link
+                    className="font-bold text-white transition transform hover:scale-105"
+                    to={link.url}
+                    key={index}
+                  >
+                    {link.text}
+                  </Link>
+                )
+              )}
+              <Link to={data.contentfulMainNavigation.ctaUrl}>
+                <button
+                  className="flex items-center font-bold text-white px-4 py-2 rounded hover:shadow transition group"
+                  style={{
+                    background: `${data.contentfulMainNavigation.ctaColor}`,
+                  }}
+                >
+                  {data.contentfulMainNavigation.ctaText}
+                  <img
+                    className="h-3 w-3 mt-0.5 ml-2 transform transition group-hover:translate-x-1"
+                    src={arrow}
+                    alt=""
+                  />
+                </button>
+              </Link>
+            </nav>
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
