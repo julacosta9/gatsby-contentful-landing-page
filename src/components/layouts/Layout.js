890f7e632df0be54f14d66a12a5bb5fc91218fac
Julian Acosta <julacosta9@gmail.com>
2021-11-06 01:26:13 -0700

    add overflow-hidden

--- src/components/layouts/Layout.js
+++ src/components/layouts/Layout.js
@@ -3,7 +3,7 @@ import Navbar from "../sections/Navbar";
 
 const Layout = ({ mainNavigationData, children }) => {
   return (
-    <div className="flex flex-col relative min-h-screen mx-0 my-auto">
+    <div className="flex flex-col relative overflow-hidden min-h-screen mx-0 my-auto">
       <div className="flex-grow mx-6">
         <Navbar data={mainNavigationData} />
         {children}
