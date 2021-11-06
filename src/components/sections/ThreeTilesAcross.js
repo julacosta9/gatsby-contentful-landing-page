0a72a72792be4040c7ce48b8e0031f5e52acd50c
Julian Acosta <julacosta9@gmail.com>
2021-11-06 14:40:18 -0700

    reduce vertical spacing between tiles when stacked

--- src/components/sections/ThreeTilesAcross.js
+++ src/components/sections/ThreeTilesAcross.js
@@ -4,7 +4,7 @@ import Tile from "./Tile";
 const ThreeTilesAcross = ({ data }) => {
   return (
     <section className="max-w-screen-content w-full mx-auto">
-      <div className="flex flex-col lg:flex-row gap-24 lg:gap-8 lg:gap-y-12 text-white py-20">
+      <div className="flex flex-col lg:flex-row gap-20 lg:gap-8 lg:gap-y-12 text-white py-20">
         {data.map((tile) => (
           <Tile
             title={tile.title}
